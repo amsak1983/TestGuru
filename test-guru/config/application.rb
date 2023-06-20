57acf80c66d2ddd819e23d469def43d184ed5e6b
amsak1983 <amsak@yandex.by>
2023-06-20 16:50:18 +0300

    2 commit

--- test-guru/config/application.rb
+++ test-guru/config/application.rb
@@ -10,8 +10,8 @@ module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.i18n.default_locale = :en
-    config.time_zone = Time.now.zone
+    config.i18n.default_locale = :ru
+    config.time_zone = 'Minsk'
 
     # Configuration for the application, engines, and railties goes here.
     #
