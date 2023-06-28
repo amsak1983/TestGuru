73152b9a82119a4baac460a3daf5e5fbafce8662
amsak1983 <amsak@yandex.by>
2023-06-28 22:10:20 +0300

    lesson3_3 commit

--- test-guru/db/migrate/20230625184358_create_tests.rb
+++ test-guru/db/migrate/20230625184358_create_tests.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class CreateTests < ActiveRecord::Migration[7.0]
   def change
     create_table :tests do |t|
       t.string :title, null: false
       t.integer :level, null: false
       t.string :name_autor, null: false
-
+      
       t.timestamps
     end
   end
--- test-guru/db/schema.rb
+++ test-guru/db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,45 +12,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_25_200058) do
-  create_table "answers", force: :cascade do |t|
-    t.string "body", null: false
-    t.boolean "correct", default: false, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "question_id"
-    t.index ["question_id"], name: "index_answers_on_question_id"
+ActiveRecord::Schema[7.0].define(version: 20_230_628_165_610) do
+  create_table 'answers', force: :cascade do |t|
+    t.string 'body', null: false
+    t.boolean 'correct', default: false, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'question_id'
+    t.index ['question_id'], name: 'index_answers_on_question_id'
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'title', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
 
-  create_table "questions", force: :cascade do |t|
-    t.string "body", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "test_id"
-    t.index ["test_id"], name: "index_questions_on_test_id"
+  create_table 'questions', force: :cascade do |t|
+    t.string 'body', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'test_id'
+    t.index ['test_id'], name: 'index_questions_on_test_id'
   end
 
-  create_table "tests", force: :cascade do |t|
-    t.string "name_autor", null: false
-    t.string "title", null: false
-    t.integer "level", default: 0, null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "category_id"
-    t.index ["category_id"], name: "index_tests_on_category_id"
+  create_table 'tests', force: :cascade do |t|
+    t.string 'title', null: false
+    t.integer 'level', default: 0, null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.integer 'category_id'
+    t.index ['category_id'], name: 'index_tests_on_category_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.integer "score", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name', null: false
+    t.integer 'score', null: false
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
