e30667f4fa0ccdcd534ab90656e5e70d0b9d3969
amsak1983 <amsak@yandex.by>
2023-06-28 22:10:20 +0300

    lesson3_3 commit

--- test-guru/db/migrate/20230625200000_add_references.rb
+++ test-guru/db/migrate/20230625200000_add_references.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal: true
+
 class AddReferences < ActiveRecord::Migration[7.0]
   def change
     add_reference(:tests, :category, null: false)
     add_reference(:questions, :test, null: false)
     add_reference(:answers, :question, null: false)
+    add_reference(:tests, :users, null: false)
   end
 end
--- test-guru/db/migrate/20230625200058_add_default_level_correct.rb
+++ test-guru/db/migrate/20230625200058_add_default_level_correct.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddDefaultLevelCorrect < ActiveRecord::Migration[7.0]
   def change
     change_column_default(:tests, :level, 0)
