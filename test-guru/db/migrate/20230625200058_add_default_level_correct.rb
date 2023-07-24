5f01fd2d3625f37f20c0715ad6ec189ea04bd119
amsak1983 <amsak@yandex.by>
2023-07-24 11:32:02 +0300

    1

--- test-guru/db/migrate/20230625200058_add_default_level_correct.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class AddDefaultLevelCorrect < ActiveRecord::Migration[7.0]
-  def change
-    change_column_default(:tests, :level, 0)
-    change_column_default(:answers, :correct, false)
-  end
-end
--- test-guru/test/models/categorie_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'test_helper'
-
-class CategorieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
