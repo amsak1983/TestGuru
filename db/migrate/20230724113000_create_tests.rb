d1052e0c3930cff250a386d39ba9165b8f92a256
amsak1983 <amsak@yandex.by>
2023-07-24 16:01:59 +0300

    b

---
+++ db/migrate/20230724113000_create_tests.rb
@@ -0,0 +1,12 @@
+class CreateTests < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tests do |t|
+      t.string :title, null: false
+      t.integer :level, null: false, default: 0
+      t.belongs_to :category, null: false, foreign_key: true
+      t.belongs_to :autor, null: false, foreign_key: { to_table: :user }
+
+      t.timestamps
+    end
+  end
+end
