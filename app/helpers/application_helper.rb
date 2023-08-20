856446d50986dbb652ec6e821866af759a7dfd15
amsak1983 <amsak@yandex.by>
2023-08-20 13:36:12 +0300

    step8

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def corrent_year
+    Time.now.year
+  end
+
+  def github_url(autor, repo)
+    link_to autor, repo, target: '_blank', rel: 'nofollow noopener'
+  end
 end
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+
+  def qustion_header(question)
+    if question.persisted?
+      "Edit #{question.test.title} Question"
+    else
+      "Create New #{@test.title} Question"
+    end
+  end
 end
