ddf2790713a2b525cbc123846e64cf1845f106b6
amsak1983 <amsak@yandex.by>
2023-11-21 18:42:47 +0300

    step1_8

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,71 +1,70 @@
 # frozen_string_literal: true
 
-module Admin
-  class TestsController < Admin::BaseController
-    before_action :set_tests, only: %i[index update_inline]
-    before_action :find_test, only: %i[show edit update destroy start update_inline]
+class Admin::TestsController < Admin::BaseController
 
-    rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :find_test, only: %i[show edit update destroy start update_inline]
 
-    def index
-      @tests = Test.all
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_test_not_found
+
+  def index
+    @tests = Test.all
+  end
+
+  def show; end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit; end
+
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
     end
+  end
 
-    def show; end
+  def create
+    @test = current_user.created_tests.build(test_params)
 
-    def new
-      @test = Test.new
+    if @test.save
+      redirect_to [:admin, @test], notice: t('.success')
+    else
+      render :new
     end
+  end
 
-    def edit; end
-
-    def update_inline
-      if @test.update(test_params)
-        redirect_to admin_tests_path
-      else
-        render :index
-      end
+  def update
+    if @test.update(test_params)
+      redirect_to [:admin, @test], notice: t('.updated')
+    else
+      render :edit
     end
+  end
 
-    def create
-      @test = current_user.created_tests.build(test_params)
+  def destroy
+    @test.destroy
+    redirect_to admin_tests_path, notice: t('.deleted')
+  end
 
-      if @test.save
-        redirect_to [:admin, @test], notice: t('.success')
-      else
-        render :new
-      end
-    end
+  private
 
-    def update
-      if @test.update(test_params)
-        redirect_to [:admin, @test], notice: t('.updated')
-      else
-        render :edit
-      end
-    end
+  def set_tests
+    @tests = Test.all
+  end
 
-    def destroy
-      @test.destroy
-      redirect_to admin_tests_path, notice: t('.deleted')
-    end
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
+  end
 
-    private
+  def find_test
+    @test = Test.find(params[:id])
+  end
 
-    def set_tests
-      @tests = Test.all
-    end
-
-    def test_params
-      params.require(:test).permit(:title, :level, :category_id)
-    end
-
-    def find_test
-      @test = Test.find(params[:id])
-    end
-
-    def rescue_with_test_not_found
-      render plain: 'Test not found!'
-    end
+  def rescue_with_test_not_found
+    render plain: 'Test not found!'
   end
 end
