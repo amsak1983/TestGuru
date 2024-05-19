0ada7723132e279c709c04125b0e2032de0a5d8d
amsak1983 <amsak@yandex.by>
2024-05-19 13:31:14 +0300

    step4_2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Test_Guru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'https//testguru-207.onrender.com' }
+  config.action_mailer.default_url_options = { host: 'https//testguru-1.onrender.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
