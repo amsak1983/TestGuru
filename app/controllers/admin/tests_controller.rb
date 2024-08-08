5eefcc1008d703f22a685f489e7ea2c4049f8bc5
amsak1983 <amsak@yandex.by>
2024-08-08 20:59:53 +0300

    step4_10

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
-class Admin
-  class TestsController < Admin::BaseController
+class Admin::TestsController < Admin::BaseController
     before_action :set_tests, only: %i[index update_inline]
     before_action :find_test, only: %i[show edit update destroy start update_inline]
 
