f6c21f878afcad6bb581f0a66930a9cb79cb32bd
amsak1983 <amsak@yandex.by>
2024-08-21 18:59:54 +0300

    step4_17

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,59 +1,57 @@
 # frozen_string_literal: true
 
-module Admin
-  class QuestionsController < Admin::BaseController
-    before_action :find_test, only: %i[new create]
-    before_action :find_question, only: %i[show edit update destroy]
+class Admin::QuestionsController < Admin::BaseController
+  before_action :find_test, only: %i[new create]
+  before_action :find_question, only: %i[show edit update destroy]
 
-    # rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+  #rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
-    def show; end
+  def show; end
 
-    def new
-      @question = Question.new
+  def new
+    @question = Question.new
+  end
+
+  def edit; end
+
+  def create
+    @question = @test.questions.build(question_params)
+
+    if @question.save
+      redirect_to [:admin, @question], notice: t('.success')
+    else
+      render :new
     end
+  end
 
-    def edit; end
-
-    def create
-      @question = @test.questions.build(question_params)
-
-      if @question.save
-        redirect_to [:admin, @question], notice: t('.success')
-      else
-        render :new
-      end
+  def update
+    if @question.update(question_params)
+      redirect_to [:admin, @question], notice: t('.updated')
+    else
+      render :edit
     end
+  end
 
-    def update
-      if @question.update(question_params)
-        redirect_to [:admin, @question], notice: t('.updated')
-      else
-        render :edit
-      end
-    end
+  def destroy
+    @question.destroy
+    redirect_to admin_test_path(@question.test), notice: t('.deleted')
+  end
 
-    def destroy
-      @question.destroy
-      redirect_to admin_test_path(@question.test), notice: t('.deleted')
-    end
+  private
 
-    private
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
 
-    def find_test
-      @test = Test.find(params[:test_id])
-    end
+  def find_question
+    @question = Question.find(params[:id])
+  end
 
-    def find_question
-      @question = Question.find(params[:id])
-    end
+  def question_params
+    params.require(:question).permit(:body)
+  end
 
-    def question_params
-      params.require(:question).permit(:body)
-    end
-
-    def rescue_with_question_not_found
-      render plain: 'Question not found!'
-    end
+  def rescue_with_question_not_found
+    render plain: 'Question not found!'
   end
 end
