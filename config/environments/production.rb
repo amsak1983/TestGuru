d0356b301aa34bd381252c02e048f20e1b1f131a
amsak1983 <amsak@yandex.by>
2024-10-13 17:59:46 +0300

    step21

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: 'smtp-relay.sendinblue.com',
+    address: 'smtp.gmail.com',
     port: 587,
     user_name: ENV['USERNAME'],
     password: ENV['PASSWORD'],
